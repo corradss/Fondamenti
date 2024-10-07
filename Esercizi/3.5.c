4c82193ec4011a5e84af9086b2a0d8c8168beafd
corradss <corradoverdino2005@gmail.com>
2024-10-07 14:56:03 +0200

    es_3.5

--- Esercizi/3.5.c
+++ Esercizi/3.5.c
@@ -9,17 +9,19 @@ int main(){
     printf("Inserisci i lati del triangolo separati da uno spazio: ");
     scanf("%f %f %f", &a, &b, &c);
 
-    if (a == b && b == c){
-        printf("Il triangolo è equilatero.\n");
-    }
-    else if (a == b || a == c || b == c){
-        printf("Il triangolo è isoscele.\n");
-    }
-    else if (a != b && b != c && a != c){
-        printf("Il triangolo è scaleno.\n");
-    }
+    if (a > 0 && b > 0 && c > 0){
+        if (a == b && b == c){
+            printf("Il triangolo è equilatero.\n");
+        }
+        else if (a == b || a == c || b == c){
+            printf("Il triangolo è isoscele.\n");
+        }
+        else if (a != b && b != c && a != c){
+            printf("Il triangolo è scaleno.\n");
+        }
 
-    if ((a*a + b*b == c*c) || ((c*c + b*b == a*a) || ((a*a + c*c == b*b)))){
-        printf("Il triangolo è rettangolo.\n");
+        if ((a*a + b*b == c*c) || ((c*c + b*b == a*a) || ((a*a + c*c == b*b)))){
+            printf("Il triangolo è rettangolo.\n");
+        }
     }
 }
