a557fe2225a72d623e5ebf986b4532b24890cc19
corrado <corradoverdino2005@gmail.com>
2024-11-19 12:24:23 +0100

    palle

--- Esercizi/esercitazione10.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("%d\n", somma(3, 5));
-}
--- Laboratorio/3/grande giove.c
+++ Laboratorio/3/grande giove.c
@@ -8,6 +8,18 @@ typedef struct{
     int minuto;
 }data;
 
+int bisestile(int anno){
+    if (anno % 400 == 0){
+        return 1;
+    }
+    else if ((anno % 4 == 0) && (anno % 100 != 0)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
 int precendenza(data partenza, data arrivo){
     if (partenza.anno > arrivo.anno){return 1;}
     else if (partenza.anno < arrivo.anno){return 0;}
@@ -31,7 +43,7 @@ int precendenza(data partenza, data arrivo){
 
 data delta_t(data partenza, data arrivo){
     int min, ore, giorni, mesi, anni;
-    int riporto;
+    int riporto, i;
     riporto = 0;
     if (arrivo.minuto - partenza.minuto < 0){riporto = -1; min = 60 - (partenza.minuto - arrivo.minuto);}
     else{min = arrivo.minuto - partenza.minuto;}
@@ -43,6 +55,14 @@ data delta_t(data partenza, data arrivo){
     else{mesi = arrivo.mese - partenza.mese + riporto; riporto = 0;}
     if (arrivo.anno - partenza.anno < 0){anni = partenza.anno - arrivo.anno + riporto; riporto = -1;}
     else{anni = arrivo.anno - partenza.anno + riporto;}
+
+    if (bisestile(partenza.anno) && partenza.mese < 2){giorni++;}
+    if (bisestile(partenza.anno) && partenza.mese == 2 && partenza.giorno <= 28){giorni++;}
+    
+    if (bisestile(arrivo.anno) && arrivo.mese >= 2 && arrivo.giorno != 29){giorni++;}
+    for (i = 1; i < (arrivo.anno - partenza.anno); i++){
+        if (bisestile(partenza.anno + i)){giorni++;}
+    }
     data tempoTrascorso;
     tempoTrascorso.anno = anni;
     tempoTrascorso.mese = mesi;
